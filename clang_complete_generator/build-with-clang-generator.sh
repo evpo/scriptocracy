53bd715345f722bf38efa71988d4085d32502785
evpo <software@evpo.net>
2023-06-16 12:55:56 +1000

    Add parameters for convenience

--- clang_complete_generator/build-with-clang-generator.sh
+++ clang_complete_generator/build-with-clang-generator.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
-if [[ "$1" == "--help" ]]; then
-    echo "$0 [compiler_name] [output_path] [make_command]" >&2
-    echo "compiler_name: example g++" >&2
-    echo "make_command: example make -j 4" >&2
-    exit -1
-fi
+function usage()
+{
+    cat <<EOM
+$(basename $0) [--use-cmake] [--cmake-cmd <CMD>] [--make-cmd <CMD>] [--compiler-name <CMD>] [-o <OUT>] [--help]
+EOM
+}
 
-compiler_name=${1:-g++}
-output_path=${2:-compiler_options.txt}
-make_command=${3:-make}
+script_dir="$( readlink ${BASH_SOURCE} )" || scriptdir="${BASH_SOURCE}"
+script_dir="$( realpath ${script_dir} )"
+script_dir="$( dirname -- $script_dir )"
 
+use_cmake=0
+cmake_command="cmake .."
+make_command="make -i"
+compiler_name="g++"
+output_path="./compiler_options.txt"
+
+options=$(getopt --longoptions "use-cmake,cmake-cmd:,make-cmd:,compiler-name:,help,h" "o:h" : "$@")
+eval set -- $options
+while true; do
+    case "$1" in
+        --use-cmake)
+            use_cmake=1
+            ;;
+        --cmake-cmd)
+            shift
+            cmake_command="$1"
+            use_cmake=1
+            ;;
+        --make-cmd)
+            shift
+            make_command="$1"
+            ;;
+        --compiler-name)
+            shift
+            compiler_name="$1"
+            ;;
+        -o)
+            shift
+            output_path="$1"
+            ;;
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
+
+
+echo $output_path
+echo $cmake_command
 echo "compiler_name=$compiler_name"
 echo "output_path=$output_path"
 echo "make_command=$make_command"
+echo "use_cmake=$use_cmake"
 
 proxy_name=$(basename $compiler_name)
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 tmp_dir=$(mktemp -d /tmp/compile_receiver.XXXXXXXX)
 pipe_path=$tmp_dir/pipe
 
@@ -39,4 +84,14 @@ mkfifo $pipe_path
 python3 $script_dir/receive-compiler.py $pipe_path $output_path &
 receive_pid=$!
 trap "finish" EXIT
-PATH=${tmp_dir}:$PATH eval $make_command
+
+export PATH=${tmp_dir}:$PATH
+if [[ ${use_cmake} == 1 ]]; then
+    eval $cmake_command
+fi
+eval $make_command
+
+output_dir=$(dirname -- $output_path)
+if [[ ! -r "${output_dir}/.ycm_extra_conf.py" ]]; then
+    cp ${script_dir}/.ycm_extra_conf.py ${output_dir}/
+fi
